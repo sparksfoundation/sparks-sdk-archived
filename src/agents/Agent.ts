98045b585bb2f185593ece19e62d7910591461ae
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-06-12 01:29:18 -0400

    add interface

--- src/agents/Agent.ts
+++ src/agents/Agent.ts
@@ -1,4 +1,6 @@
-export class Agent {
+import { IAgent } from './types.js';
+
+export class Agent implements IAgent {
   protected spark: any; // TODO define spark interface
   constructor(spark: any) {
     this.spark = spark;
