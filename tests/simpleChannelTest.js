12f73210af6da782b31c8e2a66e379af99461391
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-06-14 21:57:29 -0400

    provide simple example

--- tests/simpleChannelTest.js
+++ tests/simpleChannelTest.js
@@ -1,21 +1,35 @@
 import { Spark, Blake3, Random, Ed25519, X25519SalsaPoly, User, Verifier, PostMessage } from '../dist/index.js';
-import MockWindow from './mocks/MockWindow.js';
-const windwowA = new MockWindow('http://localhost:1111');
-const windwowB = new MockWindow('http://localhost:0000');
-const website = new Spark({ agents: [User, Verifier], controller: Random, signer: Ed25519, hasher: Blake3, cipher: X25519SalsaPoly, channels: [PostMessage] });
-await website.controller.incept();
-const alice = new Spark({ agents: [User, Verifier], controller: Random, signer: Ed25519, hasher: Blake3, cipher: X25519SalsaPoly, channels: [PostMessage] });
-await alice.controller.incept();
+import { _0000, _1111 } from './testWindows.js';
 
+const website = new Spark({ 
+  agents: [Verifier], 
+  controller: Random, 
+  signer: Ed25519, 
+  hasher: Blake3, 
+  cipher: X25519SalsaPoly, 
+  channels: [PostMessage] 
+});
+await website.controller.incept();
 
 website.channels.PostMessage.receive(async ({ resolve }) => {
   const channel = await resolve();
   channel.onmessage = res => {
     console.log(res)
   }
-}, website, windwowA);
+}, website, _1111);
 
+const alice = new Spark({ 
+  agents: [User], 
+  controller: Random, 
+  signer: Ed25519, 
+  hasher: Blake3, 
+  cipher: X25519SalsaPoly, 
+  channels: [PostMessage] 
+});
+await alice.controller.incept();
 
-const channels = new alice.channels.PostMessage({ _window: windwowB })
+const channels = new alice.channels.PostMessage({ _window: _0000 })
 const channel = await channels.open({ url: 'http://localhost:1111' })
-channel.message('hey website')
+console.log('channel receipt: ', channel.receipt)
+const receipt = await channel.message('hey website')
+console.log('message receipt: ', receipt)
---
+++ tests/testWindows.js
@@ -0,0 +1,5 @@
+import MockWindow from './mocks/MockWindow.js';
+const _0000 = new MockWindow('http://localhost:0000');
+const _1111 = new MockWindow('http://localhost:1111');
+
+export { _0000, _1111 }
