ff275e7241cdde0e00676608940b0fc0879c8531
paul pomerleau <paul.m.pomerleau@gmail.com>
2023-07-17 16:02:52 -0400

    rm old code

--- dist/channels/typesold.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.CoreChannelActions = void 0;
-const CoreChannelActions = ["OPEN", "CLOSE", "MESSAGE"];
-exports.CoreChannelActions = CoreChannelActions;
